279405f6931e4a0c03421cfd1ccafe54e0406204
John Rees <john@bitsushi.com>
2014-10-28 18:16:25 +0100

    done more

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,3 +1,11 @@
 class Admin::AdminController < ApplicationController
   layout 'admin'
+  before_filter :check_admin
+
+private
+
+  def check_admin
+    raise ActionController::RoutingError.new('Not Found') unless current_user and current_user.has_role? :admin
+  end
+
 end
