6e14e16857fbc7cf9def4b1d08c66481554658f2
John Rees <john@bitsushi.com>
2014-12-11 18:52:00 +0100

    ordinal

--- app/models/tutor_member.rb
+++ app/models/tutor_member.rb
@@ -2,6 +2,8 @@ class TutorMember < ActiveRecord::Base
   belongs_to :user
   belongs_to :course
 
+  default_scope { order(:ordinal) }
+
   def role
     role_name.present? ? role_name : "Tutor"
   end
